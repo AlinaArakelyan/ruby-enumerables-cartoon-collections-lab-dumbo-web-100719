7e4e7e3d56307fee4e5795a268c9b71c8b5d2151
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-30 19:43:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,12 +7,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  short_words = array.select do |word| word.length < 4
-  if short_words
-    return false
-  end 
-  return true
-  end 
+  # short_words = array.select do |word| word.length < 4
+  # if short_words
+  #   return false
+  # end 
+  # return true
+  # end 
+  
 end
 
 def find_the_cheese# code an argument here
